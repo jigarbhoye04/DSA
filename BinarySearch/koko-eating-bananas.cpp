26210ae388ea49751d0396d147dd44bdd5ea82c0
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-08-26 00:32:01 +0530

    Create koko-eating-bananas.cpp

---
+++ BinarySearch/koko-eating-bananas.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class Solution {
+public:
+    int findMax(vector<int> &v) {
+        int maxi = INT_MIN;
+        int n = v.size();
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, v[i]);
+        }
+        return maxi;
+    }
+
+    long long calculateTotalHours(vector<int> &v, int hourly) {
+        long long totalH = 0;
+        int n = v.size();
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += ceil((double)(v[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+
+    int minEatingSpeed(vector<int> v, int h) {
+        int low = 1, high = findMax(v);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long long totalH = calculateTotalHours(v, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
+
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution obj;
+    return 0;
+}
