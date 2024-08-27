f0120d93d1999fd1bcc5426c5e857b25cbf05ef9
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-08-27 23:45:00 +0530

    Create longest-substring-without-repeating-characters.cpp

---
+++ BinarySearch/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int lo=0,hi=0,maxLen=0;
+        set<char>st;
+
+        while(hi<n){
+            if(st.find(s[hi])==st.end()){
+                st.insert(s[hi]);
+                maxLen = max(maxLen,hi-lo+1);
+                hi++;
+            }else{
+                st.erase(s[lo]);
+                lo++;
+            }
+        }
+        return maxLen;
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution obj;
+    string s="abcabcbb";
+    // cout<<s<<endl;
+    int res = obj.lengthOfLongestSubstring(s);
+    cout<<res<<endl;
+    return 0;
+}
+
+
+//LC: https://leetcode.com/problems/longest-substring-without-repeating-characters/
